b7345f6f60aae762969ede39bc7486132ef0da9e
StewardRighteous <110706656+StewardRighteous@users.noreply.github.com.>
2024-08-01 12:39:27 +0530

    add game controller

--- script.js
+++ script.js
@@ -27,6 +27,7 @@ function createGameBoard() {
         if (isJoined.rowStatus || isJoined.columnStatus || isJoined.diagonalStatus) {
             gameOver = true;
         }
+        return gameOver;
     }
     // Checks if one of the rows has same marker 
     function isRowJoined() {
@@ -61,20 +62,42 @@ function createGameBoard() {
     return { addMarkerXO, getWinner, isGameOver };
 }
 
-function createPlayer() {
+function createPlayer(playerName, playerMarker) {
     let playerName;
     let playerMarker;
-    function setPlayerName(name) {
-        playerName = name;
-    }
     function getPlayerName() {
         return playerName;
-    }
-    function setPlayerMarker(marker) {
-        playerMarker = playerMarker;
     }
     function getPlayerMarker() {
         return playerMarker;
     }
-    return { setPlayerMarker, getPlayerMarker, setPlayerName, getPlayerName };
+    function selectIndex(){
+        let indexValue = prompt("Enter an Index Value");
+        return indexValue;
+    }
+    return { getPlayerMarker, getPlayerName, selectIndex };
+}
+
+function gameController(){
+    const totalRounds = 3;
+    const eachRoundWinner = [];
+    let roundCounter = 0;
+    player1Name = prompt("Player 1 name : ");
+    player2Name = prompt("Player 2  name :");
+    player1 = createPlayer(player1Name, "X");
+    player2 = createPlayer(player2Name , "O");
+    function startGame(){
+        while(roundCounter != totalRounds){
+            gameBoard = createGameBoard();
+            if(!gameBoard.gameOver()){
+                let indexValue = player1.selectIndex();
+                gameBoard.addMarkerXO(indexValue, player1.marker);
+                if(gameBoard.gameOver()){
+                    break;
+                }
+                indexValue =player2.selectIndex();
+                gameBoard.addMarkerXO(indexValue, player2.marker);
+            }
+        }
+    }   
 }
